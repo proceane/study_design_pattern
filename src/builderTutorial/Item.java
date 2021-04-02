7dbac3219df420feb7445246eef655cb23815e13
proceane <liteup115@gmail.com>
2021-04-02 22:19:36 +0900

    first commit

---
+++ src/builderTutorial/Item.java
@@ -0,0 +1,8 @@
+package builderTutorial;
+
+public interface Item {
+	
+	public String name();
+	public Packing packing();
+	public float price();
+}
---
+++ src/builderTutorial/Packing.java
@@ -0,0 +1,7 @@
+package builderTutorial;
+
+public interface Packing {
+	
+	public String pack();
+
+}

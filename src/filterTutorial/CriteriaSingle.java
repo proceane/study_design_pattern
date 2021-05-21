69c0b5d3f89de982f8153717fdf7cec99c80f9af
proceane <proceane1@gmail.com>
2021-05-21 13:03:08 +0900

    filter pattern study

---
+++ src/filterTutorial/CriteriaSingle.java
@@ -0,0 +1,24 @@
+package filterTutorial;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 3. Criteria인터페이스를 구현하는 클래스 생성
+ * Person이 싱글이면 리스트에 추가
+ * @date 		: 2021. 5. 21.
+ */
+public class CriteriaSingle implements Criteria {
+
+	@Override
+	public List<Person> meetCriteria(List<Person> persons) {
+		List<Person> singlePersons = new ArrayList<Person>();
+		for(Person person : persons) {
+			if(person.getMaritalStatus().equalsIgnoreCase("SINGLE")) {
+				singlePersons.add(person);
+			}
+		}
+		return singlePersons;
+	}
+
+}

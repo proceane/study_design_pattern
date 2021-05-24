bcf2b1aa92a0be8579fad512606a4a87e0d88cb3
proceane <proceane1@gmail.com>
2021-05-24 15:33:03 +0900

    facade pattern study

---
+++ src/facadeTutorial/FacadePatternDemo.java
@@ -0,0 +1,18 @@
+package facadeTutorial;
+
+/**
+ * 4. 메인메소드 생성
+ * @date 		: 2021. 5. 24.
+ */
+public class FacadePatternDemo {
+
+	public static void main(String[] args) {
+		//ShapeMaker가 객체 여러개를 한번에 만들어주는 역할을 하는듯
+		ShapeMaker shapeMaker = new ShapeMaker();
+		
+		shapeMaker.drawCircle();
+		shapeMaker.drawRectangle();
+		shapeMaker.drawSquare();
+	}
+
+}
---
+++ src/facadeTutorial/Rectangle.java
@@ -0,0 +1,14 @@
+package facadeTutorial;
+
+/**
+ * 2. Shape 구현 클래스 생성
+ * @date 		: 2021. 5. 24.
+ */
+public class Rectangle implements Shape {
+
+	@Override
+	public void draw() {
+		System.out.println("Rectangle :: draw()");
+	}
+
+}

0f831a6910b84c39a0238bf0ac59e5377b7a5d27
proceane <proceane1@gmail.com>
2021-05-26 17:14:08 +0900

    iterator pattern

---
+++ src/iteratorTutorial/Container.java
@@ -0,0 +1,9 @@
+package iteratorTutorial;
+
+/**
+ * 1. container 인터페이스 생성
+ * @date 		: 2021. 5. 26.
+ */
+public interface Container {
+	public Iterator getIterator();
+}
---
+++ src/iteratorTutorial/Iterator.java
@@ -0,0 +1,10 @@
+package iteratorTutorial;
+
+/**
+ * 1. iterator 인터페이스 생성
+ * @date 		: 2021. 5. 26.
+ */
+public interface Iterator {
+	public boolean hasNext();
+	public Object next();
+}
